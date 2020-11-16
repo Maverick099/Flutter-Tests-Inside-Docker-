0789a14f2cfff69d4e11d8ba5c85b4de20e0c536
MavericK099 <adithyashetty032@gmail.com>
2020-11-16 13:35:30 +0530

    Added command lines arguments input to file

--- main.py
+++ main.py
@@ -2,27 +2,64 @@ import os
 import platform
 import subprocess
 import re
+import argparse
 
 import scripts.runTests
+import scripts.startEmulator
 
-checkPlatform = platform.system()
-print(checkPlatform)
-CMD = ''
-if checkPlatform == 'Windows':
-    CMD = os.popen('flutter doctor')
-else:
-    subprocess.run(['flutter', 'doctor'])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p", "--path",
+        help='Add the file path here, only if test folder is located other than repo/',
+        type=str,
+        default=r'home/tester/repo',
+    )
+    parser.add_argument(
+        "-f", "--file",
+        help='Add the test file name/s with .dart extension for running test, if name is other than `widget_test.dart`',
+        type=str,
+        nargs='+',
+        default=['widget_test.dart'],
+    )
+    parser.add_argument(
+        "-d", "--debug",
+        help='Turn on Debugging mode',
+        action='store_true'
+    )
+    parser.add_argument(
+        '-e', '--emulator',
+        help='Start default the emulator',
+        action='store_true'
+    )
+    args = parser.parse_args()
+    checkPlatform = platform.system()
+    if args.debug:
+        print('[D] Platform: {}'.format(checkPlatform))
 
-line = CMD.read().splitlines()
-version = re.findall(r'\d+.\d+.\d+', line[1])
-splitLine = re.split(r'\W+', line[1])
+    if checkPlatform == 'Windows':
+        cmd = os.popen('flutter doctor')
+        lines = cmd.read().splitlines()
+        version = re.findall(r'\d+.\d+.\d+', lines[1])
+        splitLine = re.split(r'\W+', lines[1])
+        if args.debug:
+            print('[D] Flutter Version: {}'.format(splitLine[0]))
 
-if splitLine[1] == 'âˆš':
-    print('[:)] Flutter Installed:\n\tchannel: {}\n\tversion: {}'.format(splitLine[4], version[0]))
-else:
-    print('[!] Flutter not installed.....')
+        if args.emulator:
+            scripts.startEmulator.launch_emulator_win()
 
-# startEmulator.launch_emulator_win()
-scripts.runTests.run_flutter_tests_win(r'D:\work_src\test\flutter test\cli_test_app')
-# ToDo: Create a function to download flutter sdk if not found
-# ToDo: create a function to download android sdk if not found
+        scripts.runTests.run_flutter_tests_win(path=args.path, test_file=args.file, debug=args.debug)
+
+    else:
+        p = subprocess.Popen(['flutter', 'doctor'])
+        out, err = p.communicate()
+        lines = out.decode("utf-8").splitlines()
+        version = re.findall(r'\d+.\d+.\d+', lines[1])
+        splitLine = re.split(r'\W+', lines[1])
+        if args.debug:
+            print('[D] Flutter Version: {}'.format(splitLine[0]))
+
+        if args.emulator:
+            scripts.startEmulator.launch_emulator_linux()
+
+        scripts.runTests.run_flutter_tests_linux(path=args.path, test_file=args.file, debug=args.debug)
